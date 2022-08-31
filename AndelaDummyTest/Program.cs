557e88b3a6c3c866b8438d0a3e13b361b3ba9b63
saim sohail <saim@outsourcenz.com>
2022-08-31 19:01:31 +0100

    changed assignment

--- AndelaDummyTest/Program.cs
+++ AndelaDummyTest/Program.cs
@@ -1,38 +1,133 @@
-﻿using AndelaDummyTest.Entities;
-using AndelaDummyTest.Service;
-
-var cities = new List<City> {
-new City { Id = 1, Name = "New York", Coordinates = new Coordinates{ Longitude = 5, Latitude = 5} },
-new City { Id = 2, Name = "Los Angeles", Coordinates = new Coordinates{ Longitude = 10, Latitude = 2} },
-new City { Id = 3, Name = "Boston", Coordinates = new Coordinates{ Longitude = 12, Latitude = 4} },
-new City { Id = 4, Name = "Chicago", Coordinates = new Coordinates{ Longitude = 8, Latitude = 7} },
-new City { Id = 5, Name = "San Francisco", Coordinates = new Coordinates{ Longitude = 1, Latitude = 1} },
-new City { Id = 6, Name = "Washington", Coordinates = new Coordinates{ Longitude = 3, Latitude = 12} },
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace Viagogo
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            var events = new List<Event>{
+new Event{ Name = "Phantom of the Opera", City = "New York"},
+new Event{ Name = "Metallica", City = "Los Angeles"},
+new Event{ Name = "Metallica", City = "New York"},
+new Event{ Name = "Metallica", City = "Boston"},
+new Event{ Name = "LadyGaGa", City = "New York"},
+new Event{ Name = "LadyGaGa", City = "Boston"},
+new Event{ Name = "LadyGaGa", City = "Chicago"},
+new Event{ Name = "LadyGaGa", City = "San Francisco"},
+new Event{ Name = "LadyGaGa", City = "Washington"}
 };
-var events = new List<Event>{
-new Event{ Name = "Phantom of the Opera", CityId = cities.FirstOrDefault(x=>x.Name == "New York" ).Id},
-new Event{ Name = "Metallica", CityId = cities.FirstOrDefault(x=>x.Name == "Los Angeles" ).Id},
-new Event{ Name = "Metallica", CityId = cities.FirstOrDefault(x=>x.Name == "New York" ).Id},
-new Event{ Name = "Metallica", CityId = cities.FirstOrDefault(x=>x.Name == "Boston" ).Id},
-new Event{ Name = "LadyGaGa", CityId = cities.FirstOrDefault(x=>x.Name == "New York" ).Id},
-new Event{ Name = "LadyGaGa", CityId = cities.FirstOrDefault(x=>x.Name == "Boston" ).Id},
-new Event{ Name = "LadyGaGa", CityId = cities.FirstOrDefault(x=>x.Name == "Chicago" ).Id},
-new Event{ Name = "LadyGaGa", CityId = cities.FirstOrDefault(x=>x.Name == "San Francisco" ).Id},
-new Event{ Name = "LadyGaGa", CityId = cities.FirstOrDefault(x=>x.Name == "Washington" ).Id}
-};
+            
+            var customer = new Customer { Name = "Mr. Fake", City = "New York" };
+            //1.1 finding the same city
+            var sameCityEvents = events.Where(x => x.City == customer.City);
+            //1.2 Sending the email
+            
+            foreach (var item in sameCityEvents)
+            {
+                AddToEmail(customer, item);
+            }
 
-var customer = new Customer { Name = "Mr. Fake", CityId = cities.FirstOrDefault(x => x.Name == "New York").Id };
-//1.1 Get all events in the city of the customer
-var nearByEvents = events.Where(e => e.CityId == customer.CityId);
-// 1.2 Send all events to a single email TASK
+            //1.3 Only emails will be send to one client John Smith
+            //1.4 It can be improved in some ways
+            //a)Pick an event and find all customers in the same city and send them the email at once.
+            //This way by looping your events all you customers will be served.
+            //b) You can loop the customers if count of customer is not too much. Find all the events in the customer city and
+            //send that all the events in one email, but this would require changing the AddtoEmail function.
 
-//1.3 If no events then no email
-if(nearByEvents.Count()>0)
-    EventManagementService.AddEventsToEmail(customer, nearByEvents,cities.FirstOrDefault(x=>x.Id == customer.CityId).Name);
 
-//2
-EventManagementService.AddToEmail(customer,events,cities);
+            //2.
+            AddToEmail(customer, events);
+            //3 Check the statuscode of the API call, if its other than 200 then dont process the event.
+            //4 Save failed events in a queue or a table. Saved records can be processed again and can't be used by developer to figure out failure.
+            //5 We can use linq OrderBy on price to get it sorted and We can also use ThenBy to sort or multiple columns.
+        }
+        //Overloaded the method can calucated Distance by using dictionary.
+        public static void AddToEmail(Customer c, List<Event> events)
+        {
+            var interestedEvents = new List<Event>(5);
+            var minimumDistance = new Dictionary<int, double>();
+            for (int i = 0; i < events.Count(); i++)
+            {
+                var distance = GetDistance(c.City,events[i].City);
+                if (i < 5)
+                {
+                    interestedEvents.Add(events[i]);
+                    minimumDistance.Add(i, distance);
+                }
+                else
+                {
+                    var index = minimumDistance.Where(x => x.Value > distance).OrderByDescending(x => x.Value).FirstOrDefault();
+                    if (index.Value > 0)
+                    {
+                        interestedEvents[index.Key] = events[i];
+                        minimumDistance[index.Key] = distance;
+                    }
+
+                }
+
+
+            }
+
+            if (interestedEvents.Count() > 0)
+            {
+                Console.Out.WriteLine($"{c.Name} following are the events near you : ");
+                for (int i = 0; i < interestedEvents.LongCount(); i++)
+                    Console.Out.WriteLine($"{c.Name}: {interestedEvents[i].Name} in {interestedEvents[i].City}"
+                    + (minimumDistance[i] > 0 ? $" ({minimumDistance[i]} miles away)" : ""));
+            }
+
+        }
+
+        // You do not need to know how these methods work
+        static void AddToEmail(Customer c, Event e, int? price = null)
+        {
+            var distance = GetDistance(c.City, e.City);
+            Console.Out.WriteLine($"{c.Name}: {e.Name} in {e.City}"
+            + (distance > 0 ? $" ({distance} miles away)" : "")
+            + (price.HasValue ? $" for ${price}" : ""));
+        }
+        static int GetPrice(Event e)
+        {
+            return (AlphebiticalDistance(e.City, "") + AlphebiticalDistance(e.Name, "")) / 10;
+        }
+        static int GetDistance(string fromCity, string toCity)
+        {
+            return AlphebiticalDistance(fromCity, toCity);
+        }
+        private static int AlphebiticalDistance(string s, string t)
+        {
+            var result = 0;
+            var i = 0;
+            for (i = 0; i < Math.Min(s.Length, t.Length); i++)
+            {
+                result += Math.Abs(s[i] - t[i]);
+            }
+            for (; i < Math.Max(s.Length, t.Length); i++)
+            {
+                result += s.Length > t.Length ? s[i] : t[i];
+            }
+            return result;
+        }
+    }
+}
 /*
-* We want you to send an email to this customer with all events in their city
-* Just call AddToEmail(customer, event) for each event you think they should get
+var customers = new List<Customer>{
+new Customer{ Name = "Nathan", City = "New York"},
+new Customer{ Name = "Bob", City = "Boston"},
+new Customer{ Name = "Cindy", City = "Chicago"},
+new Customer{ Name = "Lisa", City = "Los Angeles"}
+};
 */
